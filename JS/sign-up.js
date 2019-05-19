7da0dccc2b5593fc3fa7d4df425b2bbc936e9eec
Ryan Ansberry <ansman92@gmail.com>
2019-05-18 22:09:01 -0700

    Added form tags+styling and bound+created js file

--- JS/sign-up.js
+++ JS/sign-up.js
@@ -1,8 +1,10 @@
-console.log(document);
-const accountSlide = document.querySelector('.account-slide');
+const accountSlide = document.querySelector(".account-slide");
 const businessSlide = document.querySelector(".business-slide");
-const continueBtn = document.querySelector(".continue");
-const returnBtn = document.querySelector(".return");
+// Buttons:
+const continueBtn = document.querySelector(".btn.continue");
+const returnBtn = document.querySelector(".btn.return");
+const signUpBtn = document.querySelector(".sign-up");
+
 continueBtn.addEventListener('click', (e) => {
     businessSlide.classList.remove("hidden");
     accountSlide.classList.add("hidden");
