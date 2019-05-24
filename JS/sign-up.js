f2fb6971cfacd9c1f4bcebff16875eb69b6b2711
Ryan Ansberry <ansman92@gmail.com>
2019-05-23 17:52:33 -0700

    Reformatted sign up forms and began js

--- JS/sign-up.js
+++ JS/sign-up.js
@@ -5,10 +5,18 @@ const continueBtn = document.querySelector(".btn.continue");
 const returnBtn = document.querySelector(".btn.return");
 const signUpBtn = document.querySelector(".sign-up");
 
+const userData = {};
+
 continueBtn.addEventListener('click', (e) => {
     businessSlide.classList.remove("hidden");
     accountSlide.classList.add("hidden");
+    userData.name = document.forms["sign-up-form"]["name"].value;
+    userData.phone = document.forms["sign-up-form"]["phone"].value;
+    userData.email = document.forms["sign-up-form"]["email"].value;
+    userData.password = document.forms["sign-up-form"]["password"].value;
+    userData.repeatPassword = document.forms["sign-up-form"]["name"].value;
 });
+
 returnBtn.addEventListener('click', (e) => {
     businessSlide.classList.add("hidden");
     accountSlide.classList.remove("hidden");
